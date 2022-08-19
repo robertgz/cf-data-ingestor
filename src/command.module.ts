9dd3a6853d3db43f2561a275d4990a5681c52f4d
Robert <robertgz@users.noreply.github.com>
2022-08-18 18:47:56 -0700

    Modifies main.ts to use nest-commander Adds CommandModule with SampleCommand Comments out root GET request test

---
+++ src/command.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { SampleCommand } from './sample-command';
+
+@Module({
+  imports: [],
+  providers: [SampleCommand],
+})
+export class CommandModule {}
